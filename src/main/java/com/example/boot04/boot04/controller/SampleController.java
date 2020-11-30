c54913b51f1bc616339cd351268312df4bc26315
hyeonwook <lastindus@gmail.com>
2020-12-01 01:46:33 +0900

    finish boot4 application

--- src/main/java/com/example/boot04/boot04/controller/SampleController.java
+++ src/main/java/com/example/boot04/boot04/controller/SampleController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -32,5 +34,41 @@ public class SampleController {
             list.add(new MemberVO(123,"u0"+i, "p0"+i, "hong"+i, new Timestamp(System.currentTimeMillis())));
         }
         model.addAttribute("list", list);
+    }
+
+    @GetMapping("/sample4")
+    public void sample4(Model model) {
+        List<MemberVO> list = new ArrayList<>();
+
+        for(int i=0; i<10; i++) {
+            list.add(new MemberVO(
+                    i,
+                    "u000" + i%3,
+                    "p0000" + i%3,
+                    "honggildong" + i,
+                    new Timestamp(System.currentTimeMillis())
+            ));
+        }
+
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping("/sample7")
+    public void sample7(Model model) {
+        model.addAttribute("now", new Date());
+        model.addAttribute("price", 123456789);
+        model.addAttribute("title", "This is a just sample");
+        model.addAttribute("options", Arrays.asList("AAA","BBBB","CCCC","DDDD"));
+
+    }
+
+    @GetMapping("/sample8")
+    public void sample8(Model model) {
+
+    }
+
+    @GetMapping("/sample/hello")
+    public void hello() {
+
     }
 }
